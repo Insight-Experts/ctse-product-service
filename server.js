810c7f31292805b24b79f39735bd98f114cbdd9c
ThaminduChankana <thaminduchankana@gmail.com>
2024-04-21 18:24:33 +0530

    added new sonar properties

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const productRoutes = require("./routes/productRoutes");
-const connectDB = require("./config/db");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+app.disable('x-powered-by');
 
 dotenv.config();
+
+const connectDB = require("./config/db");
 connectDB();
+
 app.use(express.json());
+
+const productRoutes = require("./routes/productRoutes");
+app.use("/", productRoutes);
 
 app.get("/app", (req, res) => {
   res.send("Product API is Running");
 });
 
-app.use("/", productRoutes);
-
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5003;
-app.listen(PORT, console.log(`Product Server Started on port ${PORT}..`));
+const server = app.listen(PORT, () => {
+  console.log(`Product Server Started on port ${PORT}..`);
+});
+
+process.on('SIGINT', () => {
+  console.log('Shutting down gracefully...');
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(0);
+  });
+});
